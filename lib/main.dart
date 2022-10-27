c53690961ae113f4cbbd16104f36cb22939925f8
MockAirplane700 <sizibamthandazo@yahoo.com>
2022-10-26 22:35:03 -0500

    Iteration 2 production

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,7 @@ import 'package:liber/pages/login.dart';
 import 'package:liber/persistence/mongo_database.dart';
 
 void main() async{
-  // Websites websites = Websites();
-  // websites.implementDatabase();
-  // websites.insertWebsite(InterestingWebsite(
-  //     networkImage: 'https://cdn.freeriderhd.com/free_rider_hd/sprites/frhd_1200x1200_v2.png', name: 'Free rider ',
-  //     description: 'lorem ipsum', datePublished: '26 March 2013',
-  //     source: 'https://www.tiktok.com/@setupspawn/video/7125453778920279342?is_copy_url=1&is_from_webapp=v1',
-  //     websiteUrl: 'https://www.freeriderhd.com/create',
-  //     id: 3
-  // ));
+
   WidgetsFlutterBinding.ensureInitialized();
   await MongoDatabase.connect();
   FavouritesLogic.initiateFavouritesDatabase();
